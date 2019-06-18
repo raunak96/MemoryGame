9dbb74050997a69ad1401db69fd22fc314791737
EC2 Default User <ec2-user@ip-172-31-41-28.us-east-2.compute.internal>
2019-06-18 19:20:05 +0000

    Updates

---
+++ precache-manifest.e1bb446c6058723ba08ebdab3478e870.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdbc099c6457a41ae69b09a0ab6e3ce9",
+    "url": "/MemoryGame/index.html"
+  },
+  {
+    "revision": "5182389ee0cc1d6491d9",
+    "url": "/MemoryGame/static/css/main.d4491213.chunk.css"
+  },
+  {
+    "revision": "d0581df5327a23d736c5",
+    "url": "/MemoryGame/static/js/2.3c931148.chunk.js"
+  },
+  {
+    "revision": "5182389ee0cc1d6491d9",
+    "url": "/MemoryGame/static/js/main.e315a3ff.chunk.js"
+  },
+  {
+    "revision": "4c3bd79ed0e125202499",
+    "url": "/MemoryGame/static/js/runtime~main.d54f135b.js"
+  }
+]);
